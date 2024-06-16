06a295cacd6d9bfdacb95615da29dc3e9c80d81e
Maks Mishin <maks.mishinFZ@gmail.com>
2024-06-16 19:48:31 +0300

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <iomanip>
+#include <set>
+
+using namespace std;
+
+// Генерация графа/портрета разреженной матрицы по заданной сетке
+void Generate() {
+}
+
+// Заполнение разреженной матрицы по заданному портрету
+void Fill() {
+}
+
+// Решение СЛАУ с полученной матрицей
+void Solve() {
+}
+
+// Проверка корректности программы и выдача измерений
+void Report() {
+}
+
+int main()
+{
+	return 0;
+}
